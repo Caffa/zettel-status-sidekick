69faecdb9189ae63e86a2aec1ca1331bba203510
caffae <wangyeelin@gmail.com>
2025-04-24 11:11:27 +0800

    updated license

--- prepareForGitRelease.sh
+++ prepareForGitRelease.sh
@@ -23,3 +23,5 @@ mv zettel-status-sidekick.zip release.zip
 git add -A
 git commit -m "Prepare for Git Release"
 git push origin main
+
+echo 'gh release create 1.0.4 release.zip main.js manifest.json styles.css --title "Aesthetics" --notes "Improve aesthetics"'
